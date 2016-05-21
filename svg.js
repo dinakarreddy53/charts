eb3878891d7562e742a3f7abd559e896e1b5db17
Dinakar Reddy Vallam <dinakarreddy53@gmail.com>
2016-05-21 21:49:55 +0530

    Test Changes

--- svg.js
+++ svg.js
@@ -1,8 +1,26 @@
 window.onload = function() {
     var svg_graphs = (function() {
-        var svgElement = function() {
-            alert('attributes' in this);
-            alert(this.attributes());
+        var graph = 'line';
+        var xDimensin = '600px';
+        var yDimension = '250px';
+        var initialize = function() {
+            //finding the container dimensions
+            //calculating the view box coordinates and x-axis and y-axis coordinates
+            //Testing with the percentage and then testing with the pixcel values
+        
+        };
+        
+        var Graphs = ['Line', 'Hybrid', 'BarCharts'];
+
+        var Elements = function() {
+            if('undefined' != typeof(graph) ) {
+                
+            }
+        };
+        
+        Elements.prototype.svgElement = function() {
+            if('attributes' in this)
+                this.attributes();
             var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
             svg.setAttribute('style', 'border: 1px solid black');
             svg.setAttribute('width', '600');
@@ -11,18 +29,41 @@ window.onload = function() {
             var svgElement = document.getElementById('svg_graph');
             svgElement.appendChild(svg);
         };
-        svgElement.prototype.setEvents = function() {
+        
+        Elements.prototype.gElement = function() {
+
+        };
+        
+        Elements.prototype.lineElement = function() {
+            
+        };
+        
+        Elements.prototype.pElement = function() {
+            
+        };
+        
+        Elements.prototype.circleElement = function() {
+            
+        };
+        
+        Elements.prototype.markerElement = function() {
+            
+        };
+        
+        Elements.prototype.setEvents = function() {
             alert('Events Set');
-        }
-        svgElement.prototype.attributes = function() {
+        };
+        
+        Elements.prototype.attributes = function() {
             //svgElement.prototype.setEvents();
             //svgElement.prototype.setEvents.call(this);
-            svgElement.prototype.setEvents.apply(this);
+            Elements.prototype.setEvents.apply(this);
             return 'Test';
-        }
+        };
+        
         return {
-            svgEle: new svgElement()
-        }
+            svgEle: new initialize()
+        };
     }());
     svg_graphs.svgEle;
-}
+};
